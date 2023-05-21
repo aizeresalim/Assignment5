f500993a58cc85f323f40fb3d6a205770ed11e2d
aizeresalim <0424aizeka@gmail.com>
2023-05-21 19:43:40 +0600

    iterable showcase in main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
 import java.util.Iterator;
 
 public class Main {
+    public static void main(String[] args) {
+
+        BinarySearchTree<Integer, String> bst = new BinarySearchTree();
+
+        for (int i = 0; i < 12; i++) {
+            bst.put(i, "Value " + i);
+        }
+        Iterable it = bst.iterator();
+        for(Object el: it) {
+            System.out.println(el);
+        }
+    }
+}
